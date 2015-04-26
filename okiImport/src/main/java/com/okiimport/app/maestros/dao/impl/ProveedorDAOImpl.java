a7fc0c99f072ecae60dea00745bbc6793e9b58c1
Usuario <eugeniohernandez17@gmail.com>
2015-04-26 15:01:48 -0430

    Funcion del DAO para Vilmarys que consulta los proveedores de acuerdo a los id de la clasificacion de los repuestos. El servicio no esta realizado. ProveedorDAO -> consultarProveedoresListaClasificacionRepuesto Parametros de la funcion: 1. Persona persona: persona filtro para la grid. 2. String fieldSort: campo del proveedor para el ordenamiento. 3. Boolean sortDirection: campo que indica la direccion de ordenamiento. 4. List<Integer> idsClasificacionRepuesto: ids de las clasificaciones seleccionadas, da error si el arreglo esta vacio, debe indicarse por lo menos un valor por defecto: -1. 5. int start: comienzo del registro: pagina*limit. 6. int limit: limite de registros.

--- okiImport/src/main/java/com/okiimport/app/maestros/dao/impl/ProveedorDAOImpl.java
+++ okiImport/src/main/java/com/okiimport/app/maestros/dao/impl/ProveedorDAOImpl.java
@@ -1,7 +1,12 @@
 package com.okiimport.app.maestros.dao.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 
 import com.okiimport.app.maestros.dao.ProveedorDAO;
@@ -21,6 +26,31 @@ public class ProveedorDAOImpl extends PersonaDAOImpl<Proveedor> implements Prove
 		// TODO Auto-generated method stub
 		Proveedor proveedor = (persona==null) ? new Proveedor() : new Proveedor(persona);
 		return consultarPersonaSinUsuarios(proveedor, fieldSort, sortDirection, start, limit);
+	}
+	
+	@Override
+	public List<Proveedor> consultarProveedoresListaClasificacionRepuesto(Persona persona, String fieldSort, Boolean sortDirection,
+			List<Integer> idsClasificacionRepuesto, int start, int limit) {
+		// TODO Auto-generated method stub
+		//1. Creamos el Criterio de busqueda
+		this.crearCriteria();
+
+		//2. Generamos los Joins
+		Map<String, JoinType> entidades = new HashMap<String, JoinType>();
+		entidades.put("clasificacionRepuestos", JoinType.INNER);
+		Map<String, Join> joins = this.crearJoins(entidades);
+		
+		//3. Creamos las Restricciones de la busqueda
+		List<Predicate> restricciones = new ArrayList<Predicate>();
+		restricciones.add(joins.get("clasificacionRepuestos").get("idClasificacionRepuesto")
+				.in(idsClasificacionRepuesto));
+		Proveedor proveedor = (persona==null) ? new Proveedor() : new Proveedor(persona);
+		agregarRestriccionesPersona(proveedor, restricciones);
+		
+		//4. Creamos los campos de ordenamiento y ejecutamos
+		Map<String, Boolean> orders = new HashMap<String, Boolean>();
+		orders.put("id", true);
+		return this.ejecutarCriteria(concatenaArrayPredicate(restricciones), orders, start, limit);
 	}
 
 	@Override
