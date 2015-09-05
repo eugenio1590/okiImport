cb05cb85a7140013e714da4de114045b24a50887
Miguel Angel <miguel.nesterovsky@gmail.com>
2015-09-04 20:12:28 -0430

    arreglo detalles ver oferta

---
+++ okiImport/src/main/java/com/okiimport/app/mvvm/controladores/VerDetalleOfertaViewModel.java
@@ -0,0 +1,58 @@
+package com.okiimport.app.mvvm.controladores;
+
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.zk.ui.Component;
+
+import com.okiimport.app.maestros.servicios.SMaestros;
+import com.okiimport.app.modelo.Oferta;
+import com.okiimport.app.modelo.Requerimiento;
+
+import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
+
+public class VerDetalleOfertaViewModel extends AbstractRequerimientoViewModel {
+
+	private Requerimiento requerimiento;
+	private Oferta oferta;
+
+	// private List <Requerimiento> listaRequerimientos;
+
+	@AfterCompose
+	public void doAfterCompose(@ContextParam(ContextType.VIEW) Component view,
+			@ExecutionArgParam("requerimiento") Requerimiento requerimiento,
+			@ExecutionArgParam("oferta") Oferta oferta)
+	// Lo que obtenemos de la lista es un requerimiento no una oferta
+	{
+		super.doAfterCompose(view);
+		this.requerimiento = requerimiento;
+		this.oferta = oferta;
+
+	}
+
+	public Requerimiento getRequerimiento() {
+		return requerimiento;
+	}
+
+	public void setRequerimiento(Requerimiento requerimiento) {
+		this.requerimiento = requerimiento;
+	}
+
+	public Oferta getOferta() {
+		return oferta;
+	}
+
+	public void setOferta(Oferta oferta) {
+		this.oferta = oferta;
+	}
+
+	public SMaestros getsMaestros() {
+		return sMaestros;
+	}
+
+	public void setsMaestros(SMaestros sMaestros) {
+		this.sMaestros = sMaestros;
+	}
+
+}
