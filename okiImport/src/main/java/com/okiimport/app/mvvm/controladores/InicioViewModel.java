3c7787f0d9f7a7f8f1ec4d2c04855b5cb667aa7d
Miguel Angel <miguel.nesterovsky@gmail.com>
2015-09-24 23:34:51 -0430

    Documentacion Controlador InicioViewModel

--- okiImport/src/main/java/com/okiimport/app/mvvm/controladores/InicioViewModel.java
+++ okiImport/src/main/java/com/okiimport/app/mvvm/controladores/InicioViewModel.java
@@ -12,24 +12,50 @@ import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
 
 public class InicioViewModel extends AbstractRequerimientoViewModel {
 	
+	//GUI	
 	@Wire("#principal")
 	private Borderlayout principal;
+	
+	/**
+	 * Descripcion: Llama a inicializar la clase 
+	 * Parametros: @param view: index.zul 
+	 * Retorno: Clase Inicializada 
+	 * Nota: Ninguna
+	 * */
 	@AfterCompose
 	public void doAfterCompose(@ContextParam(ContextType.VIEW) Component view){
 		super.doAfterCompose(view);
 		registrarRequerimiento();
 	}
 	
+	/**
+	 * Descripcion: Llama a formulario para registrar solicitud de proveedor 
+	 * Parametros: @param view: index.zul 
+	 * Retorno: Formulario Proveedor 
+	 * Nota: Ninguna
+	 * */
 	@Command
 	public void registrarProveedor(){
 		insertComponent(principal.getPage(), "#mainInclude", "/WEB-INF/views/portal/formularioProveedor.zul");
 	}
 	
+	/**
+	 * Descripcion: Llama a formulario para verificar solicitud de requerimiento
+	 * Parametros: @param view: index.zul 
+	 * Retorno: Formulario Verificar Requerimiento
+	 * Nota: Ninguna
+	 * */
 	@Command
 	public void verificarRequerimiento(){
 		insertComponent(principal.getPage(), "#mainInclude", "/WEB-INF/views/portal/formularioVerificarRequerimiento.zul");
 	}
 	
+	/**
+	 * Descripcion: Llama a formulario registrar requerimiento
+	 * Parametros: @param view: index.zul 
+	 * Retorno: Formulario registrar requerimiento
+	 * Nota: Ninguna
+	 * */
 	@Command
 	public void registrarRequerimiento(){
 		insertComponent(principal.getPage(), "#mainInclude", "/WEB-INF/views/portal/formularioRequerimiento.zul");
