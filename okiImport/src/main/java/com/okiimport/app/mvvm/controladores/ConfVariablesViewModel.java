4db095d06284040fbc9abd03e4122a3dd87b9f7b
Miguel Angel <miguel.nesterovsky@gmail.com>
2015-11-28 18:03:16 -0430

    Pantalla Configuracion Variables Zul y ViewModel

---
+++ okiImport/src/main/java/com/okiimport/app/mvvm/controladores/ConfVariablesViewModel.java
@@ -0,0 +1,11 @@
+package com.okiimport.app.mvvm.controladores;
+
+import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
+
+public class ConfVariablesViewModel extends AbstractRequerimientoViewModel {
+
+	public ConfVariablesViewModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
