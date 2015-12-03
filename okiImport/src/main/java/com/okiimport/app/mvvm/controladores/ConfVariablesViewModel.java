65b60265d4587200af7e3b4d8ed3fdb4dca253ed
mvilmarys <mvilmarys@gmail.com>
2015-12-02 20:45:38 -0430

    Agregado campo Moneda en Configuracion de Variables

--- okiImport/src/main/java/com/okiimport/app/mvvm/controladores/ConfVariablesViewModel.java
+++ okiImport/src/main/java/com/okiimport/app/mvvm/controladores/ConfVariablesViewModel.java
@@ -1,16 +1,40 @@
 package com.okiimport.app.mvvm.controladores;
 
+import java.util.List;
+
+import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Bandbox;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Paging;
 import org.zkoss.zul.Textbox;
 
+import com.okiimport.app.model.DetalleOferta;
+import com.okiimport.app.model.Estado;
+import com.okiimport.app.model.Moneda;
+import com.okiimport.app.model.Proveedor;
+import com.okiimport.app.model.Requerimiento;
 import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
+import com.okiimport.app.mvvm.resource.BeanInjector;
+import com.okiimport.app.service.configuracion.SControlConfiguracion;
+import com.okiimport.app.service.transaccion.STransaccion;
 
 
 	
 public class ConfVariablesViewModel extends AbstractRequerimientoViewModel {
 
+	
+	//Servicios
+	
+	@BeanInjector("sControlConfiguracion")
+	private SControlConfiguracion sControlConfiguracion;
+	
 	
 	//GUI
 		@Wire("#txtValorlibra")
@@ -22,10 +46,36 @@ public class ConfVariablesViewModel extends AbstractRequerimientoViewModel {
 		@Wire("#txtPorcentajeIVA")
 		public Textbox txtPorcentajeIVA;
 		
+		@Wire("#bandbMoneda")
+		private Bandbox bandbMoneda;
+		
+		@Wire("#pagMonedas")
+		private Paging pagMonedas;
+		
+	//Atributos
+		private List<Moneda> listaMonedas;
+		private Moneda monedaSeleccionada;
+		
 		
 	public ConfVariablesViewModel() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+	
+	@AfterCompose
+	public void doAfterCompose(@ContextParam(ContextType.VIEW) Component view,
+			@ExecutionArgParam("Moneda") List<Moneda> listaMonedas)
+	{
+		super.doAfterCompose(view);
+		
+		
+		
+	}
+	
+	
+	
+	
 	
 	/**COMMAND*/
 	/**
@@ -43,4 +93,61 @@ public class ConfVariablesViewModel extends AbstractRequerimientoViewModel {
 		super.cleanConstraintForm();
 	}
 
+
+
+	public Bandbox getBandbMoneda() {
+		return bandbMoneda;
+	}
+
+	public void setBandbMoneda(Bandbox bandbMoneda) {
+		this.bandbMoneda = bandbMoneda;
+	}
+
+
+
+	public SControlConfiguracion getsControlConfiguracion() {
+		return sControlConfiguracion;
+	}
+
+
+	public void setsControlConfiguracion(SControlConfiguracion sControlConfiguracion) {
+		this.sControlConfiguracion = sControlConfiguracion;
+	}
+
+
+	public Paging getPagMonedas() {
+		return pagMonedas;
+	}
+
+
+	public void setPagMonedas(Paging pagMonedas) {
+		this.pagMonedas = pagMonedas;
+	}
+
+
+
+	public List<Moneda> getListaMonedas() {
+		return listaMonedas;
+	}
+
+
+	public void setListaMonedas(List<Moneda> listaMonedas) {
+		this.listaMonedas = listaMonedas;
+	}
+
+
+
+	public Moneda getMonedaSeleccionada() {
+		return monedaSeleccionada;
+	}
+
+
+	public void setMonedaSeleccionada(Moneda monedaSeleccionada) {
+		this.monedaSeleccionada = monedaSeleccionada;
+	}
+
+	
+	
+	
+	
 }
