5ec141067e2dcffacab290207a269b4406ebd8d8
Usuario <eugeniohernandez17@gmail.com>
2016-01-17 22:46:12 -0430

    Implementacion del css para el modal de la lista de proveedores al que se le enviaran las solicitudes de cotizacion.

--- okiImport/src/main/java/com/okiimport/app/mvvm/controladores/SeleccionarProveedoresViewModel.java
+++ okiImport/src/main/java/com/okiimport/app/mvvm/controladores/SeleccionarProveedoresViewModel.java
@@ -1,12 +1,14 @@
 package com.okiimport.app.mvvm.controladores;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
@@ -16,10 +18,13 @@ import org.zkoss.bind.annotation.GlobalCommand;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.SelectEvent;
+import org.zkoss.zk.ui.event.SortEvent;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Messagebox.ClickEvent;
+import org.zkoss.zul.Listheader;
+import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Paging;
 import org.zkoss.zul.Window;
 
@@ -30,17 +35,14 @@ import com.okiimport.app.model.DetalleRequerimiento;
 import com.okiimport.app.model.Proveedor;
 import com.okiimport.app.model.Requerimiento;
 import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
+import com.okiimport.app.mvvm.constraint.CustomConstraint;
 import com.okiimport.app.mvvm.resource.BeanInjector;
-import com.okiimport.app.service.maestros.SMaestros;
 import com.okiimport.app.service.mail.MailProveedor;
 import com.okiimport.app.service.transaccion.STransaccion;
 
-public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewModel implements EventListener<ClickEvent> {
+public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewModel implements EventListener<SortEvent> {
 	
-	//Servicios
-	@BeanInjector("sMaestros")
-	private SMaestros sMaestros;
-	
+	//Servicios	
 	@BeanInjector("sTransaccion")
 	private STransaccion sTransaccion;
 	
@@ -50,31 +52,26 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 	//GUI
 	@Wire("#winListProveedores")
 	private Window winListProveedores;
+	
 	@Wire("#pagProveedores")
 	private Paging pagProveedores;
-	@Wire("#pagProveedoresSeleccionados")
-	private Paging pagProveedoresSeleccionados;
-	@Wire ("#btn_enviar")
-	private Button btn_enviar;
 	
 	@Wire("#gridProveedores")
 	private Listbox gridProveedores;
-	@Wire("#gridProveedoresSeleccionados")
-	private Listbox gridProveedoresSeleccionados;
-	
+		
 	//Atributos
-	private static final Comparator<Proveedor> COMPR_PROVEE = Proveedor.getComparator();
+	private CustomConstraint constraintMensaje;
+	
+	private List<Proveedor> proveedores;
+	private List<Proveedor> proveedoresSeleccionados;
+	private Set<Proveedor> proveedoresSeleccionadosARegistrar;
+	private List<DetalleRequerimiento> listaDetalleRequerimientos;
+	private List <Integer> idsClasificacionRepuesto;
 	
 	private Proveedor proveedor;
 	private Cotizacion cotizacion;
-	private DetalleRequerimiento detalleRequerimiento;
-	private List<Proveedor> listaProveedores;
-	private List<Proveedor> proveedoresSeleccionados;
-	private List<Proveedor> listaProveedoresSeleccionados1;
-	private List<Proveedor> listaProveedoresSeleccionados2;
-	private List<DetalleRequerimiento> listaDetalleRequerimientos;
-	private List <Integer> idsClasificacionRepuesto;
 	private Requerimiento requerimiento;
+	
 	private Boolean enviar;
 
 	/**
@@ -87,32 +84,38 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 	public void doAfterCompose(@ContextParam(ContextType.VIEW) Component view,
 			@ExecutionArgParam("enviar") Boolean enviar,
 			@ExecutionArgParam("repuestosseleccionados") List <DetalleRequerimiento> repuestosseleccionados){
+		super.doAfterCompose(view);
 		this.enviar = enviar;
 		this.cotizacion = new Cotizacion("Estimado Proveedor le hacemos el envio de un nuevo requerimiento para su posterior revisi\u00F3n ", enviar);
 		this.listaDetalleRequerimientos = repuestosseleccionados;
-		listaProveedoresSeleccionados1 = new ArrayList<Proveedor>(); 
-		super.doAfterCompose(view);
-		limpiar();
-		pagProveedores.setPageSize(pageSize=9);
+		this.proveedoresSeleccionadosARegistrar = new LinkedHashSet<Proveedor>();
+		this.constraintMensaje = (this.enviar) ? this.getNotEmptyValidator() : null;
 		
-	
+		agregarGridSort(gridProveedores);
+		limpiar();
+		pagProveedores.setPageSize(pageSize=5);
+		
 		idsClasificacionRepuesto = new ArrayList<Integer>();
 		for(DetalleRequerimiento detalle:repuestosseleccionados){
 			requerimiento = detalle.getRequerimiento();
 			idsClasificacionRepuesto.add(detalle.getClasificacionRepuesto().getIdClasificacionRepuesto());
 		}
-		consultarProveedores(0);
+		
+		consultarProveedores(0, null, null);
 	}
 	
-	/**Interface*/
+	/**Interface: EventListener<SortEvent>*/
 	@Override
-	public void onEvent(ClickEvent event) throws Exception {
-		// TODO Auto-generated method stub
-		winListProveedores.detach();
-		ejecutarGlobalCommand("removerSeleccionados", null);
-	}
-	
-	
+	@NotifyChange({"proveedores"})
+	public void onEvent(SortEvent event) throws Exception {
+		if(event.getTarget() instanceof Listheader){
+			Map<String, Object> parametros = new HashMap<String, Object>();
+			parametros.put("fieldSort", ((Listheader) event.getTarget()).getValue().toString());
+			parametros.put("sortDirection", event.isAscending());
+			ejecutarGlobalCommand("consultarProveedores", parametros );
+		}
+		
+	}	
 	/**GLOBAL COMMAND*/
 	
 	/**
@@ -122,22 +125,22 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 	 * Nota: Ninguna
 	 * */
 	@GlobalCommand
-	@NotifyChange({"listaProveedores"})
-	public void consultarProveedores(@Default("0") int page){
-		Map<String, Object> Parametros= sMaestros.ConsultarProveedoresListaClasificacionRepuesto(proveedor, null, null, requerimiento.getIdRequerimiento(), idsClasificacionRepuesto,page, pageSize);
-		listaProveedores = (List<Proveedor>) Parametros.get("proveedores");
+	@NotifyChange({"proveedores"})
+	public void consultarProveedores(@Default("0") int page,
+			@BindingParam("fieldSort") String fieldSort, 
+			@BindingParam("sortDirection") Boolean sortDirection){
+		agregarProveedoresSeleccionados();
+		Map<String, Object> Parametros= sMaestros.ConsultarProveedoresListaClasificacionRepuesto(proveedor, fieldSort, sortDirection, requerimiento.getIdRequerimiento(), idsClasificacionRepuesto,page, pageSize);
+		proveedores = (List<Proveedor>) Parametros.get("proveedores");
 		Integer total = (Integer) Parametros.get("total");
 		gridProveedores.setMultiple(true);
 		gridProveedores.setCheckmark(true);
-		gridProveedoresSeleccionados.setMultiple(true);
-		gridProveedoresSeleccionados.setCheckmark(true);
 		pagProveedores.setActivePage(page);
 		pagProveedores.setTotalSize(total);
 		
 	}
 	
 	/**COMMAND*/
-	
 	/**
 	 * Descripcion: Permite limpiar los campos del formulario Seleccionar Proveedores
 	 * Parametros: @param view: seleccionarProveedores.zul
@@ -150,33 +153,6 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 		proveedor = new Proveedor();
 		super.cleanConstraintForm();
 	}
-
-	/**
-	 * Descripcion: Permite seleccionar un proveedor para realizar una cotizacion
-	 * Parametros: @param view: seleccionarProveedores.zul
-	 * Retorno: Ninguno
-	 * Nota: Ninguna
-	 * */
-	@NotifyChange({"*"})
-	@Command
-	public void agregarProveedores(){
-		super.moveSelection(listaProveedores,listaProveedoresSeleccionados1, proveedoresSeleccionados, 
-				COMPR_PROVEE, false, "No se puede agregar Proveedor");
-		
-	}
-	
-	/**
-	 * Descripcion: Permite eliminar un proveedor para no cotizarle
-	 * Parametros: @param view: seleccionarProveedores.zul
-	 * Retorno: Ninguno 
-	 * Nota: Ninguna
-	 * */
-	@NotifyChange({"*"})
-	@Command
-	public void eliminarProveedores(){
-		if(listaProveedoresSeleccionados2!=null && !listaProveedoresSeleccionados2.isEmpty())
-			listaProveedoresSeleccionados1.removeAll(listaProveedoresSeleccionados2);
-	}
 	
 	/**
 	 * Descripcion: Cambia la paginacion de acuerdo a la pagina activa
@@ -185,72 +161,59 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 	 * Retorno: Ninguno
 	 * Nota: Ninguna
 	 * */
-	@NotifyChange({"*"})
 	@Command
+	@NotifyChange({"proveedores"})
 	public void paginarLista(){
-		consultarProveedores(pagProveedores.getActivePage());
-	}
-	
-	@NotifyChange({"*"})
-	@Command
-	public void paginarListaSeleccionados(){
-		consultarProveedores(pagProveedoresSeleccionados.getActivePage());
+		consultarProveedores(pagProveedores.getActivePage(), null, null);
 	}
 	
 	/**
 	 * Descripcion: Permite filtrar por los diferentes campos del formulario segun solicite el evento
-	 * Parametros: @param view: seleccionarProveedores.zul
+	 * Parametros: Ninguno
 	 * Retorno: Ninguno
 	 * Nota: Ninguna
 	 * */
-	@NotifyChange({"*"})
 	@Command
+	@NotifyChange({"proveedores"})
 	public void aplicarFiltro(){
-		consultarProveedores(0);
+		consultarProveedores(0, null, null);
+	}
+
+	/**
+	 * Descripcion: Permite capturar el evento al seleccionar un proveedor y verifica si se encuentra
+	 * dentro de la lista de seleccionados, si no esta lo elimina de los proveedores 
+	 * a enviar la solicitud de cotizacion.
+	 * Parametros: @param selectEvent: evento de seleccion
+	 * Retorno: Ninguno
+	 * Nota: Ninguna
+	 * */
+	@Command
+	public void seleccionarProveedor(@ContextParam(ContextType.TRIGGER_EVENT) SelectEvent<Listitem, Proveedor> selectEvent){
+		Listitem item = (Listitem) selectEvent.getReference();
+		if(item!=null){
+			Proveedor proveedor = proveedores.get(item.getIndex());
+			if(proveedoresSeleccionados!=null && !proveedoresSeleccionados.contains(proveedor))
+				this.proveedoresSeleccionadosARegistrar.remove(proveedor);
+		}
 	}
 	
 	/**
-	 * Descripcion: Permite enviar la solicitu de cotizacion a los proveedores
-	 * Parametros: @param view: seleccionarProveedores.zul 
+	 * Descripcion: Permite capturar el evento al seleccionar todos los proveedores y verifica si se encuentran
+	 * dentro de la lista de seleccionados, si no estan los elimina de los proveedores 
+	 * a enviar la solicitud de cotizacion.
+	 * Parametros: @param checkEvent: evento de seleccion
 	 * Retorno: Ninguno
 	 * Nota: Ninguna
 	 * */
 	@Command
-	@NotifyChange({"listaProveedoresSeleccionados1"})
-	public void enviar(){
-		if(!listaProveedoresSeleccionados1.isEmpty()){
-			if(checkIsFormValid())
-			{
-				for(Proveedor proveedor:listaProveedoresSeleccionados1){
-
-					Cotizacion cotizacion2 =  cotizacion.clon();
-					cotizacion2.setProveedor(proveedor);
-					List<DetalleCotizacion> detalleCotizacions = new ArrayList<DetalleCotizacion>();
-
-					for(DetalleRequerimiento detalleRequerimiento:listaDetalleRequerimientos){
-						DetalleCotizacion detalleCotizacion = (proveedor.getTipoProveedor()) ? new DetalleCotizacion() : new DetalleCotizacionInternacional();
-						detalleCotizacion.setDetalleRequerimiento(detalleRequerimiento);
-						detalleCotizacions.add(detalleCotizacion);
-					}
-					sTransaccion.registrarSolicitudCotizacion(cotizacion2, detalleCotizacions);
-
-					if(enviar)
-						this.mailProveedor.enviarRequerimientoProveedor(proveedor, requerimiento, detalleCotizacions, mailService);
-				}
-				btn_enviar.setDisabled(true);
-			}
-
-			mostrarMensaje("Informaci\u00F3n", "Cotizaci\u00F3n enviada Exitosamente ", null, null, this, null);
-			winListProveedores.onClose();
-		}
-		else
-			mostrarMensaje("Informaci\u00F3n", "Seleccione al menos un Proveedor ", null, null, null, null);
-
+	public void seleccionarProveedores(){
+		if(proveedoresSeleccionados==null)
+			proveedoresSeleccionadosARegistrar.removeAll(proveedores);
 	}
 	
 	/**
 	 * Descripcion: Permite registrar un proveedor llamando al modal formularioProveedor
-	 * Parametros: @param view: seleccionarProveedores.zul 
+	 * Parametros: Ninguno
 	 * Retorno: Ninguno
 	 * Nota: Ninguna
 	 * */
@@ -260,37 +223,62 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 		parametros.put("cerrar", false);
 		crearModal(BasePackagePortal+"formularioProveedor.zul", parametros);
 	}
-	
+		
 	/**
-	 * Descripcion: Permite recargar la pagina
-	 * Parametros: @param view: seleccionarProveedores.zul 
+	 * Descripcion: Permite enviar la solicitu de cotizacion a los proveedores
+	 * Parametros: Ninguno
 	 * Retorno: Ninguno
 	 * Nota: Ninguna
 	 * */
-	public void recargar() {
-		redireccionar("/");
+	@Command
+	public void enviar(@BindingParam("btnEnviar") Button btnEnviar, @BindingParam("btnLimpiar") Button btnLimpiar){
+		agregarProveedoresSeleccionados();
+		if(!this.proveedoresSeleccionadosARegistrar.isEmpty()){
+			btnEnviar.setDisabled(true);
+			btnLimpiar.setDisabled(true);
+			
+			Cotizacion cotizacion = null;
+			List<DetalleCotizacion> detalleCotizacions = null;
+			for(Proveedor proveedor : proveedoresSeleccionadosARegistrar){
+
+				cotizacion =  this.cotizacion.clon();
+				cotizacion.setProveedor(proveedor);
+				detalleCotizacions = new ArrayList<DetalleCotizacion>();
+
+				for(DetalleRequerimiento detalleRequerimiento : listaDetalleRequerimientos){
+					DetalleCotizacion detalleCotizacion = (proveedor.getTipoProveedor()) ? new DetalleCotizacion() : new DetalleCotizacionInternacional();
+					detalleCotizacion.setDetalleRequerimiento(detalleRequerimiento);
+					detalleCotizacions.add(detalleCotizacion);
+				}
+				sTransaccion.registrarSolicitudCotizacion(cotizacion, detalleCotizacions);
+
+				if(enviar)
+					this.mailProveedor.enviarRequerimientoProveedor(proveedor, requerimiento, detalleCotizacions, mailService);
+			}
+			
+			mostrarMensaje("Informaci\u00F3n", "Cotizaci\u00F3n enviada Exitosamente ", null, null, null, null);
+			closeModal();
+		}
+		else
+			mostrarMensaje("Informaci\u00F3n", "Seleccione al menos un Proveedor ", null, null, null, null);
+
+	}
+	
+	/**METODOS OVERRIDE*/
+	@Command
+	@Override
+	public void closeModal() {
+		super.closeModal();
+		ejecutarGlobalCommand("removerSeleccionados", null);
 	}
 	
     /**METODOS PROPIOS DE LA CLASE*/
-	
+	private void agregarProveedoresSeleccionados(){
+		if(proveedoresSeleccionados!=null)
+			proveedoresSeleccionadosARegistrar.addAll(proveedoresSeleccionados);
+	}
+
 	/**GETTERS Y SETTERS*/
-	
-	public Proveedor getProveedor() {
-		return proveedor;
-	}
-
-	public void setProveedor(Proveedor proveedor) {
-		this.proveedor = proveedor;
-	}
-
-	public SMaestros getsMaestros() {
-		return sMaestros;
-	}
-
-	public void setsMaestros(SMaestros sMaestros) {
-		this.sMaestros = sMaestros;
-	}
-
 	public STransaccion getsTransaccion() {
 		return sTransaccion;
 	}
@@ -306,15 +294,22 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 	public void setMailProveedor(MailProveedor mailProveedor) {
 		this.mailProveedor = mailProveedor;
 	}
-
-	public List<Proveedor> getListaProveedores() {
-		return listaProveedores;
+	
+	public CustomConstraint getConstraintMensaje() {
+		return constraintMensaje;
 	}
 
-	public void setListaProveedores(List<Proveedor> listaProveedores) {
-		this.listaProveedores = listaProveedores;
+	public void setConstraintMensaje(CustomConstraint constraintMensaje) {
+		this.constraintMensaje = constraintMensaje;
 	}
 
+	public List<Proveedor> getProveedores() {
+		return proveedores;
+	}
+
+	public void setProveedores(List<Proveedor> proveedores) {
+		this.proveedores = proveedores;
+	}
 
 	public List<Proveedor> getProveedoresSeleccionados() {
 		return proveedoresSeleccionados;
@@ -323,56 +318,13 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 	public void setProveedoresSeleccionados(List<Proveedor> proveedoresSeleccionados) {
 		this.proveedoresSeleccionados = proveedoresSeleccionados;
 	}
-
-	public List<Integer> getIdsClasificacionRepuesto() {
-		return idsClasificacionRepuesto;
+	
+	public Proveedor getProveedor() {
+		return proveedor;
 	}
 
-	public void setIdsClasificacionRepuesto(List<Integer> idsClasificacionRepuesto) {
-		this.idsClasificacionRepuesto = idsClasificacionRepuesto;
-	}
-
-	public List<Proveedor> getListaProveedoresSeleccionados1() {
-		return listaProveedoresSeleccionados1;
-	}
-
-	public void setListaProveedoresSeleccionados1(
-			List<Proveedor> listaProveedoresSeleccionados1) {
-		this.listaProveedoresSeleccionados1 = listaProveedoresSeleccionados1;
-	}
-
-	public List<Proveedor> getListaProveedoresSeleccionados2() {
-		return listaProveedoresSeleccionados2;
-	}
-
-	public void setListaProveedoresSeleccionados2(
-			List<Proveedor> listaProveedoresSeleccionados2) {
-		this.listaProveedoresSeleccionados2 = listaProveedoresSeleccionados2;
-	}
-
-	public Listbox getGridProveedores() {
-		return gridProveedores;
-	}
-
-	public void setGridProveedores(Listbox gridProveedores) {
-		this.gridProveedores = gridProveedores;
-	}
-
-	public Listbox getGridProveedoresSeleccionados() {
-		return gridProveedoresSeleccionados;
-	}
-
-	public void setGridProveedoresSeleccionados(Listbox gridProveedoresSeleccionados) {
-		this.gridProveedoresSeleccionados = gridProveedoresSeleccionados;
-	}
-
-	public List<DetalleRequerimiento> getListaDetalleRequerimientos() {
-		return listaDetalleRequerimientos;
-	}
-
-	public void setListaDetalleRequerimientos(
-			List<DetalleRequerimiento> listaDetalleRequerimientos) {
-		this.listaDetalleRequerimientos = listaDetalleRequerimientos;
+	public void setProveedor(Proveedor proveedor) {
+		this.proveedor = proveedor;
 	}
 	
 	public Cotizacion getCotizacion() {
@@ -381,14 +333,6 @@ public class SeleccionarProveedoresViewModel extends AbstractRequerimientoViewMo
 
 	public void setCotizacion(Cotizacion cotizacion) {
 		this.cotizacion = cotizacion;
-	}
-
-	public DetalleRequerimiento getDetalleRequerimiento() {
-		return detalleRequerimiento;
-	}
-
-	public void setDetalleRequerimiento(DetalleRequerimiento detalleRequerimiento) {
-		this.detalleRequerimiento = detalleRequerimiento;
 	}
 
 	public Boolean getEnviar() {
