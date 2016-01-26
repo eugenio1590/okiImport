084e39b661b07db4839a9296f6dc6c39263dd923
Usuario <eugeniohernandez17@gmail.com>
2016-01-26 00:15:30 -0430

    Implementacion del decorator y cambios de los componentes visuales. Se integro las estrategias con la logica del controlador.

--- okiImport/src/main/java/com/okiimport/app/mvvm/controladores/ListaCreacionOfertasViewModel.java
+++ okiImport/src/main/java/com/okiimport/app/mvvm/controladores/ListaCreacionOfertasViewModel.java
@@ -1,7 +1,6 @@
 package com.okiimport.app.mvvm.controladores;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -11,8 +10,9 @@ import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.bind.annotation.ExecutionArgParam;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zul.GroupsModel;
-import org.zkoss.zul.GroupsModelArray;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Tabpanels;
+import org.zkoss.zul.Tabs;
 
 import com.okiimport.app.model.DetalleCotizacion;
 import com.okiimport.app.model.DetalleOferta;
@@ -21,15 +21,30 @@ import com.okiimport.app.model.Oferta;
 import com.okiimport.app.model.Requerimiento;
 import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
 import com.okiimport.app.mvvm.resource.BeanInjector;
+import com.okiimport.app.mvvm.resource.decorator.ofertas.DecoratorTabOferta;
+import com.okiimport.app.mvvm.resource.estrategia.detalles_cotizacion.ResolveEstrategiaSortDetalleCotizacion;
 import com.okiimport.app.service.transaccion.STransaccion;
+import com.okiimport.app.service.web.SLocalizacion;
 
+@SuppressWarnings("rawtypes")
 public class ListaCreacionOfertasViewModel extends AbstractRequerimientoViewModel {
 	
 	// Servicios
 	@BeanInjector("sTransaccion")
 	private STransaccion sTransaccion;
-
+	
+	@BeanInjector("sLocalizacion")
+	private SLocalizacion sLocalizacion;
+	
+	//GUI
+	@Wire("#tabsOfertas")
+	private Tabs tabsOfertas;
+	
+	@Wire("#tabpOfertas")
+	private Tabpanels tabpOfertas;
+	
 	//Atributos
+	private ResolveEstrategiaSortDetalleCotizacion resolve;
 	private Map<DetalleRequerimiento, List<DetalleCotizacion>> listasDetalleCotizacion; //Servicio
 	private List<DetalleOferta> detallesOfertas;
 	private List<Oferta> ofertas;
@@ -49,6 +64,7 @@ public class ListaCreacionOfertasViewModel extends AbstractRequerimientoViewMode
 		super.doAfterCompose(view);
 		this.requerimiento = requerimiento;
 		this.cantOfertas = sTransaccion.consultarCantOfertasCreadasPorRequermiento(requerimiento.getIdRequerimiento());
+		this.resolve = new ResolveEstrategiaSortDetalleCotizacion(sLocalizacion);
 		crearOfertas();
 	}
 	
@@ -71,19 +87,68 @@ public class ListaCreacionOfertasViewModel extends AbstractRequerimientoViewMode
 	}
 	
 	/**METODOS PROPIOS DE LA CLASE*/
+	@SuppressWarnings("unchecked")
 	private void crearOfertas() {
+		int pos = 1; //luego se calculara
+		Oferta oferta;
+		List<DetalleCotizacion> detallesCotizacion;
+		List<DetalleRequerimiento> keys = new ArrayList<DetalleRequerimiento>();
 		ofertas = new ArrayList<Oferta>();
-		detallesOfertas = new ArrayList<DetalleOferta>();
 		listasDetalleCotizacion = sTransaccion.consultarDetallesCotizacion(requerimiento.getIdRequerimiento());
-		for(DetalleRequerimiento key: listasDetalleCotizacion.keySet()){
-			Oferta oferta = new Oferta();
-			for(DetalleCotizacion detallleC : listasDetalleCotizacion.get(key)){
-				DetalleOferta detalleOferta = new DetalleOferta(detallleC);
-				oferta.addDetalleOferta(detalleOferta );
-				detallesOfertas.add(detalleOferta);
+		
+		while(ofertas.size()<3 && pos<=6){
+			//1. Creamos una oferta nueva
+			oferta = new Oferta(ofertas.size()+1);
+			
+			//2. Actualizamos el ordenamiento de cada una de las listas
+			for(DetalleRequerimiento key: listasDetalleCotizacion.keySet()){
+				detallesCotizacion = listasDetalleCotizacion.get(key);
+				this.resolve.resolveEstrategia(detallesCotizacion, pos);
+				
+				//2.1 Agregamos la mejor opcion a la oferta respectiva
+				if(detallesCotizacion.size()>0){
+					oferta.addDetalleOferta(new DetalleOferta(detallesCotizacion.get(0)));
+					
+					//2.1.1 Removemos la mejor oferta de la lista
+					detallesCotizacion.remove(0);
+					
+					//2.1.2 Verificamos el vacio de la lista y Actualizamos
+					if(detallesCotizacion.isEmpty())
+						keys.add(key);
+				}
 			}
-			ofertas.add(oferta);
+			
+			//3. Eliminamos los keys del map
+			if(!keys.isEmpty())
+				for(DetalleRequerimiento key:keys)
+					listasDetalleCotizacion.remove(key);
+			
+			//4. Agregamos la nueva oferta a la lista de ofertas
+			if(oferta.isNotEmpty())
+				this.ofertas.add(oferta);
+			
+			//5. Actualizamos la pos de la estrategia a usar en la siguiente iteracion
+			pos++;
 		}
+		
+		DecoratorTabOferta decorator;
+		if(!ofertas.isEmpty())
+			for(Oferta oferta2 : ofertas){
+				decorator = new DecoratorTabOferta(tabsOfertas, tabpOfertas, oferta2);
+				decorator.agregarOferta(this);
+			}
+		
+//		detallesOfertas = new ArrayList<DetalleOferta>();
+//		listasDetalleCotizacion = sTransaccion.consultarDetallesCotizacion(requerimiento.getIdRequerimiento());
+//		for(DetalleRequerimiento key: listasDetalleCotizacion.keySet()){
+//			Oferta oferta = new Oferta();
+//			for(DetalleCotizacion detallleC : listasDetalleCotizacion.get(key)){
+//				DetalleOferta detalleOferta = new DetalleOferta(detallleC);
+//				oferta.addDetalleOferta(detalleOferta );
+//				detallesOfertas.add(detalleOferta);
+//			}
+//			ofertas.add(oferta);
+//		}
 	}
 
 	/**GETTERS Y SETTERS*/
@@ -93,6 +158,14 @@ public class ListaCreacionOfertasViewModel extends AbstractRequerimientoViewMode
 
 	public void setsTransaccion(STransaccion sTransaccion) {
 		this.sTransaccion = sTransaccion;
+	}
+
+	public SLocalizacion getsLocalizacion() {
+		return sLocalizacion;
+	}
+
+	public void setsLocalizacion(SLocalizacion sLocalizacion) {
+		this.sLocalizacion = sLocalizacion;
 	}
 
 	public List<Oferta> getOfertas() {
@@ -110,8 +183,4 @@ public class ListaCreacionOfertasViewModel extends AbstractRequerimientoViewMode
 	public void setDetallesOfertas(List<DetalleOferta> detallesOfertas) {
 		this.detallesOfertas = detallesOfertas;
 	}
-
-	
-	
-	
 }
---
+++ okiImport/src/main/java/com/okiimport/app/mvvm/resource/decorator/ofertas/DecoratorTabOferta.java
@@ -0,0 +1,48 @@
+package com.okiimport.app.mvvm.resource.decorator.ofertas;
+
+import org.zkoss.zk.ui.util.Template;
+import org.zkoss.zul.Include;
+import org.zkoss.zul.Tab;
+import org.zkoss.zul.Tabpanel;
+import org.zkoss.zul.Tabpanels;
+import org.zkoss.zul.Tabs;
+
+import com.okiimport.app.model.Oferta;
+import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
+
+public class DecoratorTabOferta {
+
+	//GUI
+	private Tabs tabs; //Paneles de Navegacion
+	
+	private Tabpanels tabpanels; //Contenidos
+	
+	//Atributos
+	private Oferta oferta;
+
+	public DecoratorTabOferta(Tabs tabs, Tabpanels tabpanels, Oferta oferta) {
+		super();
+		this.tabs = tabs;
+		this.tabpanels = tabpanels;
+		this.oferta = oferta;
+	}
+
+	/**METODOS PROPIOS DE LA CLASE*/
+	public void agregarOferta(AbstractRequerimientoViewModel viewModel) {
+		//Se agrego el nuevo panel de navegacion para la oferta
+		Tab newTab = new Tab();
+		newTab.setLabel("Oferta Nro. "+oferta.getNroOferta());
+		tabs.appendChild(newTab);
+		
+		//Se agrega el contenido del panel
+		Tabpanel newPanel = new Tabpanel();
+		Include include = new Include();
+		include.setSrc("/WEB-INF/views/template/decorator_oferta/decorator_oferta.zul");
+		include.setAttribute("vm", viewModel);
+		include.setAttribute("detallesOferta", oferta.getDetalleOfertas());
+		include.setAttribute("numero", 54, true);
+		newPanel.appendChild(include);
+		tabpanels.appendChild(newPanel);
+	}
+
+}
