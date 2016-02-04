aaf99a9e99e85061eca74bafa92006a2801040e5
Usuario <eugeniohernandez17@gmail.com>
2016-02-04 17:09:30 -0430

    Implementacion de las acciones sobre las ofertas en el decorator.

--- okiImport/src/main/java/com/okiimport/app/mvvm/resource/decorator/ofertas/DecoratorTabOferta.java
+++ okiImport/src/main/java/com/okiimport/app/mvvm/resource/decorator/ofertas/DecoratorTabOferta.java
@@ -1,5 +1,7 @@
 package com.okiimport.app.mvvm.resource.decorator.ofertas;
 
+import java.util.List;
+
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.Button;
 import org.zkoss.zul.Include;
@@ -10,6 +12,7 @@ import org.zkoss.zul.Tabpanel;
 import org.zkoss.zul.Tabpanels;
 import org.zkoss.zul.Tabs;
 
+import com.okiimport.app.model.DetalleOferta;
 import com.okiimport.app.model.Oferta;
 import com.okiimport.app.model.enumerados.EEstatusOferta;
 import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
@@ -56,13 +59,9 @@ public class DecoratorTabOferta {
 		tabpanels.appendChild(newPanel);
 	}
 	
-	public void updateDetalleOferta(Integer id, Button buttonAction, boolean acept){
-		StringBuilder builder;
-		
+	public void updateDetalleOferta(Integer id, Button buttonAction, boolean acept){	
 		//Row
-		builder = new StringBuilder("row").append(oferta.getNroOferta()).append("-").append(id);
-		Listitem row = findComponent(builder.toString());
-		row.setSclass((acept) ? LISTITEM_GREEN : LISTITEM_RED);
+		updateRow(id, acept);
 		
 		if(!acept){
 			oferta.setEstatus(EEstatusOferta.INVALIDA);
@@ -79,9 +78,27 @@ public class DecoratorTabOferta {
 		updateEstado(oferta.getEstatus().getNombre());
 	}
 	
+	public void updateOferta(boolean acept){
+		oferta.setEstatus((acept) ? EEstatusOferta.SELECCIONADA : EEstatusOferta.INVALIDA);
+		List<DetalleOferta> detalles = this.oferta.getDetalleOfertas();
+		for(int i=0; i<detalles.size(); i++){
+			detalles.get(i).setAprobado(acept);
+			updateRow(i, acept);
+			updateButton("acp", i, (acept) ? false : true);
+			updateButton("dec", i, (acept) ? true : false);
+		}
+		updateEstado(oferta.getEstatus().getNombre());
+	}
+	
 	private void updateEstado(String estatus){
 		Label label = findComponent("estatusOferta"+this.oferta.getNroOferta());
 		label.setValue(estatus);
+	}
+	
+	private void updateRow(Integer id, boolean acept){
+		StringBuilder builder = new StringBuilder("row").append(oferta.getNroOferta()).append("-").append(id);
+		Listitem row = findComponent(builder.toString());
+		row.setSclass((acept) ? LISTITEM_GREEN : LISTITEM_RED);
 	}
 	
 	private void updateButton(String type, Integer id, boolean visible){
