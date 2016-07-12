7d643690c6f67b4f385c105e4c2ece989732489f
Freddy <Freddy@Freddy-PC>
2016-07-12 19:39:44 -0400

    acomode la lista de ofertas que se vera al cliente desde su lista de requerimientos.

--- okiImport/src/main/java/com/okiimport/app/mvvm/controladores/cliente/requerimientos/MisRequerimientoViewModel.java
+++ okiImport/src/main/java/com/okiimport/app/mvvm/controladores/cliente/requerimientos/MisRequerimientoViewModel.java
@@ -1,13 +1,16 @@
 package com.okiimport.app.mvvm.controladores.cliente.requerimientos;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
 import org.zkoss.zk.ui.Component;
 
+import com.okiimport.app.model.Requerimiento;
 import com.okiimport.app.mvvm.controladores.AbstractMisRequerimientosViewModel;
 
 public class MisRequerimientoViewModel extends AbstractMisRequerimientosViewModel  {
@@ -30,7 +33,15 @@ public class MisRequerimientoViewModel extends AbstractMisRequerimientosViewMode
 	public void registarNuevoRequerimiento(){
 		crearModal(BasePackageSistemaFunc+"usuario/registrarRequerimiento.zul", null);
 	}
-
+	@Command
+	public void mostrarOfertas(@BindingParam("requerimiento") Requerimiento requerimiento){
+		
+		Map<String, Object> parametros = new HashMap<String, Object>();
+		parametros.put("requerimiento", requerimiento);
+		crearModal(BasePackageSistemaFunc+"usuario/listaOfertasCliente.zul", parametros);
+		
+		
+	}
 
 	/**METODOS OVERRIDE*/
 	@Override
